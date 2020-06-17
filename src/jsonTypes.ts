ebe8c3e8d3527b1c144b0c7e5719f53c54c488eb
Ricardo Devis Agullo <mkredz@gmail.com>
2020-06-17 23:44:31 +0200

    add properties on return

--- src/jsonTypes.ts
+++ src/jsonTypes.ts
@@ -6,33 +6,51 @@ interface JSONStringBrand {
 }
 
 interface StringOptions {
+  readonly description?: string;
   readonly minLength?: number;
   readonly maxLength?: number;
   readonly pattern?: string;
 }
 
-export const jsonString = ({
-  minLength = -1,
-  maxLength = Infinity,
-  pattern = '.*',
-}: StringOptions): t.BrandC<t.StringC, JSONStringBrand> =>
-  t.brand(
-    t.string,
-    (n): n is t.Branded<string, JSONStringBrand> => {
-      return (
-        n.length >= minLength &&
-        n.length <= maxLength &&
-        new RegExp(pattern).test(n)
-      );
-    },
-    'JSONString'
+interface JSONString extends t.BrandC<t.StringC, JSONStringBrand> {
+  jsonSchema: StringOptions;
+}
+
+export const jsonString = (
+  optionsOrDescription: StringOptions | string = {}
+): JSONString => {
+  const options: StringOptions =
+    typeof optionsOrDescription === 'string'
+      ? { description: optionsOrDescription }
+      : optionsOrDescription;
+
+  return Object.assign(
+    t.brand(
+      t.string,
+      (n): n is t.Branded<string, JSONStringBrand> => {
+        const {
+          minLength = -1,
+          maxLength = Infinity,
+          pattern = '.*',
+        } = options;
+        return (
+          n.length >= minLength &&
+          n.length <= maxLength &&
+          new RegExp(pattern).test(n)
+        );
+      },
+      'JSONString'
+    ),
+    { jsonSchema: options }
   );
+};
 
 interface JSONNumberBrand {
   readonly JSONNumber: unique symbol;
 }
 
 interface NumberOptions {
+  readonly description?: string;
   readonly multipleOf?: number;
   readonly minimum?: number;
   readonly exclusiveMinimum?: number;
@@ -40,54 +58,78 @@ interface NumberOptions {
   readonly exclusiveMaximum?: number;
 }
 
-export const jsonNumber = ({
-  multipleOf = 1,
-  minimum = -Infinity,
-  exclusiveMinimum = -Infinity,
-  maximum = Infinity,
-  exclusiveMaximum = Infinity,
-}: NumberOptions): t.BrandC<t.NumberC, JSONStringBrand> =>
-  t.brand(
-    t.number,
-    (n): n is t.Branded<number, JSONNumberBrand> => {
-      return (
-        n % multipleOf === 0 &&
-        n >= minimum &&
-        n > exclusiveMinimum &&
-        n <= maximum &&
-        n < exclusiveMaximum
-      );
-    },
-    'JSONNumber'
+interface JSONNumber extends t.BrandC<t.NumberC, JSONNumberBrand> {
+  jsonSchema: NumberOptions;
+}
+
+export const jsonNumber = (
+  optionsOrDescription: NumberOptions | string = {}
+): JSONNumber => {
+  const options: NumberOptions =
+    typeof optionsOrDescription === 'string'
+      ? { description: optionsOrDescription }
+      : optionsOrDescription;
+
+  return Object.assign(
+    t.brand(
+      t.number,
+      (n): n is t.Branded<number, JSONNumberBrand> => {
+        const {
+          multipleOf = 1,
+          minimum = -Infinity,
+          exclusiveMinimum = -Infinity,
+          maximum = Infinity,
+          exclusiveMaximum = Infinity,
+        } = options;
+
+        return (
+          n % multipleOf === 0 &&
+          n >= minimum &&
+          n > exclusiveMinimum &&
+          n <= maximum &&
+          n < exclusiveMaximum
+        );
+      },
+      'JSONNumber'
+    ),
+    { jsonSchema: options }
   );
+};
 
 export type JSONArray<C extends t.Mixed> = t.Type<
   Array<t.TypeOf<C>>,
   Array<t.OutputOf<C>>,
   unknown
->;
+> & { jsonSchema: ArrayJSONOptions };
 
-interface ArrayOptions<C extends t.Mixed> {
-  codec: C;
+interface ArrayJSONOptions {
+  description?: string;
   minItems?: number;
   maxItems?: number;
 }
 
-export function jsonArray<C extends t.Mixed>({
-  codec,
-  minItems = -Infinity,
-  maxItems = Infinity,
-}: ArrayOptions<C>): JSONArray<C> {
+interface ArrayOptions<C extends t.Mixed> extends ArrayJSONOptions {
+  codec: C;
+}
+
+export function jsonArray<C extends t.Mixed>(
+  options: ArrayOptions<C>
+): JSONArray<C> {
+  const { codec, ...arrayJsonOptions } = options;
+  const { minItems = -Infinity, maxItems = Infinity } = arrayJsonOptions;
   const arr = t.array(codec);
-  return new t.Type(
-    `JSONArray<${codec.name}`,
-    (u): u is Array<t.TypeOf<C>> => arr.is(u),
-    (u, c) =>
-      either.chain(arr.validate(u, c), (as) => {
-        return as.length <= maxItems && as.length >= minItems
-          ? t.success(as)
-          : t.failure(u, c);
-      }),
-    (nea) => arr.encode(nea)
+  return Object.assign(
+    new t.Type(
+      `JSONArray<${codec.name}`,
+      (u): u is Array<t.TypeOf<C>> => arr.is(u),
+      (u, c) =>
+        either.chain(arr.validate(u, c), (as) => {
+          return as.length <= maxItems && as.length >= minItems
+            ? t.success(as)
+            : t.failure(u, c);
+        }),
+      (nea) => arr.encode(nea)
+    ),
+    { jsonSchema: arrayJsonOptions }
   );
 }
